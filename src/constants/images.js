a260d97b6bb00da5212ac31aa46e5965fb15bf25
Vitaliy Mitnichuk <mitvitchuk@gmail.com>
2023-05-15 18:07:15 +0300

    main page refactor

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,11 @@
 // MAIN
 import MoneyBG from '../assets/Main/MoneyBg.png';
-
+import GitHub from '../assets/Main/GitHub.svg';
+import Piechart from '../assets/Main/PieChart.png';
 
 export default {
     // MAIN
     MoneyBG,
+    GitHub,
+    Piechart,
 }
