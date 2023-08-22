73894c33c07e561ac50f02706119e81ac1e85e0c
Vitaliy Mitnichuk <mitvitchuk@gmail.com>
2023-08-22 12:54:35 +0300

    api url 2

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getFromLocalStorage, saveToLocalStorage } from "./tokenStorage";
 import jwtDecode from "jwt-decode";
 
-const url = "https://moneyhackapi.satonteam.com/";
+const url = "https://moneyhackapi.satonteam.com/api/";
 const accessToken = getFromLocalStorage("ACCESS_TOKEN");
 const refreshToken = getFromLocalStorage("REFRESH_TOKEN");
 let token;
